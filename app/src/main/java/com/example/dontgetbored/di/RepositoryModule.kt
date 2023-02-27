4e2c57bff2da1fd58e5a5ed3317e92c20c9ac892
cirilobido <ciriloenmanuel@gmail.com>
2023-02-27 14:05:00 -0400

    :construction: Retrofit and Repos added to config

--- app/src/main/java/com/example/dontgetbored/di/RepositoryModule.kt
+++ app/src/main/java/com/example/dontgetbored/di/RepositoryModule.kt
@@ -1,11 +1,19 @@
 package com.example.dontgetbored.di
 
+import com.example.dontgetbored.data.repository.BoredActivityRepositoryImpl
+import com.example.dontgetbored.domain.repository.BoredActivityRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
-
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindBoredActivityRepository(
+        boredActivityRepositoryImpl: BoredActivityRepositoryImpl
+    ): BoredActivityRepository
 }
