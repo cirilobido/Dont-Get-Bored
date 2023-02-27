7df992b3376d073d45c5fce9abda2fdeb31a615b
cirilobido <ciriloenmanuel@gmail.com>
2023-02-27 16:32:45 -0400

    :sparkles: Main Navigation Added

--- app/src/main/java/com/example/dontgetbored/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dontgetbored/presentation/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.dontgetbored.presentation.navigation.AppNavigation
 import com.example.dontgetbored.presentation.theme.DontGetBoredTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,22 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DontGetBoredTheme {
-        Greeting("Android")
     }
 }
