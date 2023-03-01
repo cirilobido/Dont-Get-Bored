7cdb2279109ce8238f751645442abdfec431a082
cirilobido <ciriloenmanuel@gmail.com>
2023-03-01 09:26:04 -0400

    :zap: Resource response moved

--- app/src/main/java/com/example/dontgetbored/data/repository/BoredActivityRepositoryImpl.kt
+++ app/src/main/java/com/example/dontgetbored/data/repository/BoredActivityRepositoryImpl.kt
@@ -4,19 +4,11 @@ import com.example.dontgetbored.data.dto.toBoredActivity
 import com.example.dontgetbored.data.remote.BoredApi
 import com.example.dontgetbored.domain.model.BoredActivityModel
 import com.example.dontgetbored.domain.repository.BoredActivityRepository
-import com.example.dontgetbored.util.Resource
 import javax.inject.Inject
 
 class BoredActivityRepositoryImpl @Inject constructor(private val api: BoredApi):
     BoredActivityRepository {
-    override suspend fun getRandomActivity(): Resource<BoredActivityModel> {
-        return try {
-            Resource.Success(
-                data = api.getRandomActivity().toBoredActivity()
-            )
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Resource.Error(e.message ?: "An unknown error occurred.")
-        }
+    override suspend fun getRandomActivity(): BoredActivityModel {
+        return api.getRandomActivity().toBoredActivity()
     }
 }
